31fb508b0cefcd885825bebcaa0767f2a5e16e09
psixapat222226 <SvetSvet228@yandex.ru>
2025-12-15 18:04:30 +0300

    2

--- vue-practice-27/vite.config.js
+++ vue-practice-27/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/frontend-and-backend-vue-5/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
